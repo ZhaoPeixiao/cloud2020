66a2dc8e4fd150bd295887f3142b59a35abe626d
Peixiao Zhao <u6935249@anu.edu.au>
2020-12-05 18:50:15 +0800

    12.5

---
+++ cloud-consumer-order80/src/main/java/com/peixiao/springcloud/OrderMain.java
@@ -0,0 +1,15 @@
+package com.peixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/12/5 18:06
+ */
+@SpringBootApplication
+public class OrderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain.class, args);
+    }
+}
