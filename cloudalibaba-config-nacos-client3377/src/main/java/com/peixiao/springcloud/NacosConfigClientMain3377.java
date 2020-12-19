68ae604d3093df4370111be1dca7cf2ffc4a0e77
Peixiao Zhao <u6935249@anu.edu.au>
2020-12-19 16:01:36 +0800

    12.19

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/peixiao/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.peixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/12/19 15:25
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
+
