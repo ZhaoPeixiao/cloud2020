e8b286607a35719a264b4df94a4d4a551e96c5f6
Peixiao Zhao <u6935249@anu.edu.au>
2020-12-24 09:12:22 +0800

    12.24

---
+++ cloudalibaba-provider-payment9003/src/main/java/com/peixiao/springcloud/PaymentMain9003.java
@@ -0,0 +1,17 @@
+package com.peixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/12/24 8:33
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9004/src/main/java/com/peixiao/springcloud/PaymentMain9004.java
@@ -0,0 +1,17 @@
+package com.peixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/12/24 8:36
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
